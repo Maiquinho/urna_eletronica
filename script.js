0d4c6e588631eed0f154105273810f0add1f8fb3
Michael Matheus <michaelmatheus2001@gmail.com>
2022-07-07 22:54:08 -0300

    feat: candidate update infos into javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+let seuVotoPara = document.querySelector('.tela__divisao-1-voto span');
+
+let cargo = document.querySelector('.tela__divisao-1-politico span');
+
+let descricao = document.querySelector('.tela__divisao-1-informacoes');
+
+let aviso = document.querySelector('.tela__divisao-2');
+
+let lateral = document.querySelector('.tela__divisao-1-right');
+
+let numeros = document.querySelector('.tela__divisao-1-caixa-numeros');
